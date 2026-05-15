d3a7414f769c860b71f4662f2471cefb6bc1ff6b
joaosavioli06 <joaogabrielsavioli6@gmail.com>
2026-05-15 10:51:59 -0300

    fix(layout): ajuste para app iniciar na tela de login

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -2,13 +2,48 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
+import { useEffect } from 'react';
+import { useFonts } from 'expo-font';
+import {useRouter, useSegments } from 'expo-router';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 import { RegisterProvider } from '@/contexts/RegisterContext';
 import { DiscardProvider } from '@/contexts/DiscardContext';
 import { AuthProvider } from '@/contexts/AuthContext';
 
+import * as SplashScreen from 'expo-splash-screen';
+import { useAuth } from '@/contexts/AuthContext'; // Garanta que o caminho está correto
+
+// app/_layout.tsx
+
 export const unstable_settings = {
+  // 1. Mudamos o 'anchor' para o grupo de autenticação
+  initialRouteName: '(auth)', 
+};
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <AuthProvider>
+      <DiscardProvider>
+        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+          <RegisterProvider>
+            <Stack initialRouteName="(auth)">
+              {/* 2. Declaramos o (auth) PRIMEIRO aqui na lista */}
+              <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+              <Stack.Screen name="modal" />
+            </Stack>
+          </RegisterProvider>
+          <StatusBar style="auto" />
+        </ThemeProvider>
+      </DiscardProvider>
+    </AuthProvider>
+  );
+}
+
+/*export const unstable_settings = {
   anchor: '(tabs)',
 };
 
@@ -20,7 +55,7 @@ export default function RootLayout() {
       <DiscardProvider>
         <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
 
-          <RegisterProvider>
+        <RegisterProvider>
             <Stack>
               <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
               <Stack.Screen name="(auth)" options={{ headerShown: false }} />
@@ -34,3 +69,4 @@ export default function RootLayout() {
     </AuthProvider>
   );
 }
+*/
